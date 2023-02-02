6f4304538c55136ffa967e62b161c955d062eda2
fabio <fabio.moriero@strumenta.com>
2023-02-02 19:25:26 +0100

    [WIP] Adding walk method for traversing the AST

---
+++ SharpLasu.Tests/TraversingTest.cs
@@ -0,0 +1,44 @@
+﻿using Strumenta.Sharplasu.Tests.Models;
+using Strumenta.Sharplasu.Validation;
+
+namespace Strumenta.Sharplasu.Tests
+{
+
+    [TestClass]
+    public class TraversingTest
+    {
+        [TestMethod]
+        public void TestWalk() {
+            var parser = new ExampleSharpLasuParser();
+            var cu = parser.GetTreeForText("display 6.12");
+            // TODO ...
+        }
+
+        [TestMethod]
+        public void CheckParseTreeWithErrors() {
+            var parser = new ExampleSharpLasuParser();
+            var cu = parser.GetTreeForText("displar 12.3");
+
+            Assert.IsFalse(cu.Correct);
+            Assert.IsNotNull(cu.Root);
+        }
+
+        [TestMethod]
+        public void CheckASTWithSemanticError() {
+            var parser = new ExampleSharpLasuParser();
+            var cu = parser.GetTreeForText(
+@"set a = 2
+display 12.3
+set b = 0"
+);
+
+            Assert.IsFalse(cu.Correct);
+            Assert.AreEqual(1, cu.Issues.Count);
+            Assert.AreEqual("Display statement not supported", cu.Issues[0].Message);
+            Assert.AreEqual(IssueType.SEMANTIC, cu.Issues[0].IssueType);
+
+            Assert.IsInstanceOfType(cu.Root, typeof(CompilationUnit));
+            Assert.AreEqual(2, cu.Root.Statements.Count);
+        }
+    }
+}
---
+++ SharpLasu/Traversing/Traversing.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Strumenta.Sharplasu.Model;
+
+namespace Strumenta.Sharplasu.Traversing
+{
+}
+
+namespace ExtensionMethods
+{
+    public static class TraversingExtensions
+    {
+        /**
+         * Traverse the entire tree, deep first, starting from this Node
+         */
+        public static IEnumerable<Node> walk(this Node node) {
+            Stack<Node> stack = new Stack<Node>();
+            stack.Push(node);
+
+            if (stack.Count == 0) {
+                yield return null;
+            }
+            else {
+                var next = stack.Pop();
+                next.Children.ForEach(child => stack.Push(child));
+                yield return next;
+            }
+        }
+    }
+}
