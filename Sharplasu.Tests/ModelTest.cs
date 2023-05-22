8ea3a16bb8077037b296e751b82cf09f61f9d59b
Gabriele Tomassetti <gabriele@strumenta.com>
2023-05-22 17:10:51 +0200

    Porting ReferenceByName from Kolasu

---
+++ Sharplasu.Tests/ModelTest.cs
@@ -0,0 +1,71 @@
+﻿using Strumenta.Sharplasu.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Strumenta.Sharplasu.Tests
+{
+    [TestClass]
+    public class ModelTest
+    {
+        private class MyNode : Node, Named
+        {
+            public string Name { get; set; }
+
+            public MyNode(string name)
+            { 
+                Name = name;
+            }            
+        }
+
+        [TestMethod]
+        public void ReferenceByNameUnsolvedToString()
+        {
+            var refUnsolved = new ReferenceByName<MyNode>("foo");
+            Assert.AreEqual("Ref(foo)[Unsolved]", refUnsolved.ToString());
+        }
+
+        [TestMethod]
+        public void referenceByNameSolvedToString()
+        {
+            var refSolved = new ReferenceByName<MyNode>("foo", new MyNode("foo"));
+            Assert.AreEqual("Ref(foo)[Solved]", refSolved.ToString());
+        }
+
+        
+        [TestMethod]
+        public void TryToResolvePositiveCaseSameCase()
+        {
+            var reference = new ReferenceByName<MyNode>("foo");
+            Assert.AreEqual(true, reference.TryToResolve(new List<MyNode>() { new MyNode("foo") }));
+            Assert.AreEqual(true, reference.Resolved);
+        }
+        
+        [TestMethod]
+        public void TryToResolveNegativeCaseSameCase()
+        {
+            var reference = new ReferenceByName<MyNode>("foo");
+            Assert.AreEqual(false, reference.TryToResolve(new List<MyNode>() { new MyNode("foo2") }));
+            Assert.AreEqual(false, reference.Resolved);
+        }
+
+        [TestMethod]
+        public void TryToResolvePositiveCaseDifference()
+        {
+            var reference = new ReferenceByName<MyNode>("foo");
+            Assert.AreEqual(true, reference.TryToResolve(new List<MyNode>() { new MyNode("fOo") }, caseInsensitive: true));
+            Assert.AreEqual(true, reference.Resolved);
+        }
+
+        [TestMethod]
+        public void TryToResolveNegativeCaseDifference()
+        {
+            var reference = new ReferenceByName<MyNode>("foo");
+            Assert.AreEqual(false, reference.TryToResolve(new List<MyNode>() { new MyNode("fOo") }));
+            Assert.AreEqual(false, reference.Resolved);
+        }
+    }
+}
---
+++ Sharplasu/Model/Naming.cs
@@ -0,0 +1,137 @@
+﻿using Strumenta.Sharplasu.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Xml.Linq;
+using static Antlr4.Runtime.Atn.SemanticContext;
+
+namespace Strumenta.Sharplasu.Model
+{
+    /**
+     * An entity which has a name.
+     */
+    public interface Named
+    {
+        /**
+         * The mandatory name of the entity.
+         */
+        string Name { get; set; }
+    }
+
+
+    /**
+     * A reference associated by using a name.
+     * It can be used only to refer to Nodes and not to other values.
+     *
+     * This is not statically enforced as we may want to use some interface, which cannot extend Node.
+     * However, this is enforced dynamically.
+     */
+    [Serializable]
+    public class ReferenceByName<N>
+        where N : class, Named
+    {
+
+        public ReferenceByName(string name, object initialReferred = null)
+        {
+            Name = name;
+            referred = initialReferred as N;
+        }
+
+        public string Name { get; set; }
+
+        public bool Resolved
+        {
+            get
+            {
+                return this.Referred != null;
+            }
+        }
+
+        private N referred;
+        public N Referred
+        {
+            get
+            {
+                return referred;
+            }
+            set
+            {
+                if (!(value is Node) && value != null) {
+                    throw new Exception($"We cannot enforce it statically but only Node should be referred to. Instead $value was assigned (class: {value.GetType().FullName})");
+                }
+                referred = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            if (Resolved)
+            {
+                return $"Ref({Name})[Solved]";
+            }
+            else
+            {
+                return $"Ref({Name})[Unsolved]";
+            }
+        }
+
+        public override bool Equals(object obj)
+        {            
+            return obj is ReferenceByName<N> name &&
+                   Name == name.Name &&
+                   EqualityComparer<N>.Default.Equals(referred, name.referred);
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = 2021201050;
+            hashCode = hashCode * -1521134295 + EqualityComparer<String>.Default.GetHashCode(Name);
+            hashCode = hashCode * -1521134295 + EqualityComparer<Named>.Default.GetHashCode(referred);
+            return hashCode;            
+        }        
+    }
+
+    public static class NamingExtensions
+    {
+
+        /**
+         * Try to resolve the reference by finding a named element with a matching name.
+         * The name match is performed in a case sensitive or insensitive way depending on the value of @param[caseInsensitive].
+         */
+        public static bool TryToResolve<N>(this ReferenceByName<N> reference, IEnumerable<N> candidates, bool caseInsensitive = false)            
+            where N : class, Named 
+        {
+            N res = candidates.Where(it =>
+            {
+                if (it.Name == null)
+                    return false;
+                else
+                    return it.Name.Equals(reference.Name, caseInsensitive ? StringComparison.InvariantCultureIgnoreCase : StringComparison.InvariantCulture);
+            }).FirstOrDefault();
+            reference.Referred = res;
+            return res != null;
+        }
+
+        /**
+         * Try to resolve the reference by assigning @param[possibleValue]. The assignment is not performed if
+         * @param[possibleValue] is null.
+         *
+         * @return true if the assignment has been performed
+         */
+        public static bool TryToResolve<N>(this ReferenceByName<N> reference, N possibleValue)
+                where N : class, Named
+        {
+            if (possibleValue == null)
+            {
+                return false;
+            }                
+            else
+            {
+                reference.Referred = possibleValue;
+                return true;
+            }
+        }        
+    }
+}
