ab2de00de5fa693494998301dd794739695d9411
Gabriele Tomassetti <gabriele@strumenta.com>
2023-08-25 14:52:21 +0200

    Add transformation test

--- Sharplasu.Tests/ASTTransformerTests.cs
+++ Sharplasu.Tests/ASTTransformerTests.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Strumenta.Sharplasu.Testing;
-using Microsoft.VisualStudio.TestPlatform.Utilities;
 
 namespace Strumenta.Sharplasu.Tests
 {   
@@ -52,6 +51,60 @@ namespace Strumenta.Sharplasu.Tests
             }
         }
 
+        private abstract class Expression : Node { }
+        private class IntLiteral : Expression
+        {
+            public int Value { get; set;} = 0;
+
+            public IntLiteral(int value)
+            {
+                Value = value;
+            }
+        }
+
+        private enum Operator
+        {
+            PLUS, MULT
+        }
+
+        private class GenericBinaryExpression : Node
+        {
+            public Operator Operator { get; set; }
+            public Expression Left { get; set; }
+            public Expression Right { get; set; }
+
+            public GenericBinaryExpression(Operator op, Expression left, Expression right)
+            {
+                Operator = op;
+                Left = left;
+                Right = right;
+            }
+        }
+
+        private class Mult : Node
+        {
+            public Expression Left { get; set; }
+            public Expression Right { get; set; }
+
+            public Mult(Expression left, Expression right)
+            {                
+                Left = left;
+                Right = right;
+            }
+        }
+
+        private class Sum : Node
+        {
+            public Expression Left { get; set; }
+            public Expression Right { get; set; }
+
+            public Sum(Expression left, Expression right)
+            {
+                Left = left;
+                Right = right;
+            }
+        }
+
         [TestMethod]
         public void TestIdentitiyTransformer()
         {
@@ -73,6 +126,42 @@ namespace Strumenta.Sharplasu.Tests
             Asserts.AssertASTsAreEqual(cu, transformedCU, considerPosition: true);
             Assert.IsTrue(transformedCU.HasValidParents());
             Assert.AreEqual(transformedCU.Origin, cu);
+        }
+
+        [TestMethod]
+        public void TranslateBinaryExpression()
+        {
+            var transformer = new ASTTransformer(allowGenericNode: false);
+            transformer.RegisterNodeFactory<Node>(typeof(GenericBinaryExpression), (source, ast) =>
+            {
+                var sb = source as GenericBinaryExpression;         
+
+                switch (sb.Operator)
+                {
+                    case Operator.MULT:
+                        return new Mult(
+                            ast.Transform(sb.Left) as Expression,
+                            ast.Transform(sb.Right) as Expression
+                        );
+                    case Operator.PLUS:
+                        return new Sum(
+                            ast.Transform(sb.Left) as Expression,
+                            ast.Transform(sb.Right) as Expression
+                         );
+                    default:
+                        return null;
+                }
+            });
+            transformer.RegisterIdentityTransformation<IntLiteral>(typeof(IntLiteral));
+            var m = transformer.Transform(new GenericBinaryExpression(Operator.MULT, new IntLiteral(7), new IntLiteral(8)));
+            Asserts.AssertASTsAreEqual(
+                new Mult(new IntLiteral(7), new IntLiteral(8)),
+                transformer.Transform(new GenericBinaryExpression(Operator.MULT, new IntLiteral(7), new IntLiteral(8)))
+            );
+            Asserts.AssertASTsAreEqual(
+                new Sum(new IntLiteral(7), new IntLiteral(8)),
+                transformer.Transform(new GenericBinaryExpression(Operator.PLUS, new IntLiteral(7), new IntLiteral(8)))
+            );
         }
     }
 }
