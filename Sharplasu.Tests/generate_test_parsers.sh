8c85f107098d6192d9426a0ac9c932cc4385ac98
Gabriele Tomassetti <gabriele@strumenta.com>
2023-08-29 09:47:30 +0200

    Fix encoding issue

--- Sharplasu.Tests/generate_test_parsers.sh
+++ Sharplasu.Tests/generate_test_parsers.sh
@@ -1,1 +1,1 @@
-java -cp ../antlr-4.13.0-complete.jar org.antlr.v4.Tool -Dlanguage=CSharp -o Generated ./antlr4/SimpleLangLexer.g4 ./antlr4/SimpleLangParser.g4
+java -cp ../antlr-4.13.0-complete.jar org.antlr.v4.Tool -Dlanguage=CSharp -Xexact-output-dir -o Generated ./antlr4/SimpleLangLexer.g4 ./antlr4/SimpleLangParser.g4
