7d8869ee0c6c0549380c0972e70b077b3846088e
Gabriele Tomassetti <gabriele@strumenta.com>
2023-08-30 15:16:58 +0200

    Update Position to match corresponding Kolasu features

--- Sharplasu.Tests/Model/PositionTest.cs
+++ Sharplasu.Tests/Model/PositionTest.cs
@@ -1,12 +1,220 @@
-﻿using System;
+﻿using Antlr4.Runtime;
+using Strumenta.Sharplasu.Model;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Strumenta.Sharplasu.Parsing;
+using System.ComponentModel.DataAnnotations;
 
 namespace Strumenta.Sharplasu.Tests.Model
 {
-    internal class PositionTest
+    [TestClass]
+    public class PositionTest
     {
+        private class MySetStatement : Node
+        {
+            public new Position SpecifiedPosition { get; set; }
+
+            public MySetStatement(Position specifiedPosition = null) : base(specifiedPosition)
+            {
+                SpecifiedPosition = specifiedPosition;
+            }
+        }
+
+        [TestMethod]
+        public void OffsetTests()
+        {
+            var code = @"this is some code
+second line
+third line";
+            Assert.AreEqual(0, Point.START_POINT.Offset(code));
+            Assert.AreEqual(5, new Point(1, 5).Offset(code));
+            Assert.AreEqual(17, new Point(1, 17).Offset(code));
+            Assert.AreEqual(18, new Point(2, 0).Offset(code));
+            Assert.ThrowsException<InvalidOperationException>(() => new Point(1, 18).Offset(code));
+            Assert.ThrowsException<InvalidOperationException>(() => new Point(4, 0).Offset(code));
+        }
+
+        [TestMethod]
+        public void PointCompare()
+        {
+            var p0 = Point.START_POINT;
+            var p1 = new Point(1, 1);
+            var p2 = new Point(1, 100);
+            var p3 = new Point(2, 90);
+            
+            Assert.AreEqual(false, p0 < p0);
+            Assert.AreEqual(true, p0 <= p0);
+            Assert.AreEqual(true, p0 >= p0);
+            Assert.AreEqual(false, p0 > p0);
+                   
+            Assert.AreEqual(true, p0 < p1);
+            Assert.AreEqual(true, p0 <= p1);
+            Assert.AreEqual(false, p0 >= p1);
+            Assert.AreEqual(false, p0 > p1);
+                   
+            Assert.AreEqual(true, p0 < p2);
+            Assert.AreEqual(true, p0 <= p2);
+            Assert.AreEqual(false, p0 >= p2);
+            Assert.AreEqual(false, p0 > p2);
+                   
+            Assert.AreEqual(true, p0 < p3);
+            Assert.AreEqual(true, p0 <= p3);
+            Assert.AreEqual(false, p0 >= p3);
+            Assert.AreEqual(false, p0 > p3);
+                   
+            Assert.AreEqual(true, p1 < p2);
+            Assert.AreEqual(true, p1 <= p2);
+            Assert.AreEqual(false, p1 >= p2);
+            Assert.AreEqual(false, p1 > p2);
+                   
+            Assert.AreEqual(true, p1 < p3);
+            Assert.AreEqual(true, p1 <= p3);
+            Assert.AreEqual(false, p1 >= p3);
+            Assert.AreEqual(false, p1 > p3);
+        }
+
+        [TestMethod]
+        public void IsBefore()
+        {
+            var p0 = Point.START_POINT;
+            var p1 = new Point(1, 1);
+            var p2 = new Point(1, 100);
+            var p3 = new Point(2, 90);
+
+            Assert.AreEqual(false, p0.IsBefore(p0));
+            Assert.AreEqual(true, p0.IsBefore(p1));
+            Assert.AreEqual(true, p0.IsBefore(p2));
+            Assert.AreEqual(true, p0.IsBefore(p3));
+
+            Assert.AreEqual(false, p1.IsBefore(p0));
+            Assert.AreEqual(false, p1.IsBefore(p1));
+            Assert.AreEqual(true, p1.IsBefore(p2));
+            Assert.AreEqual(true, p1.IsBefore(p3));
+
+            Assert.AreEqual(false, p2.IsBefore(p0));
+            Assert.AreEqual(false, p2.IsBefore(p1));
+            Assert.AreEqual(false, p2.IsBefore(p2));
+            Assert.AreEqual(true, p2.IsBefore(p3));
+
+            Assert.AreEqual(false, p3.IsBefore(p0));
+            Assert.AreEqual(false, p3.IsBefore(p1));
+            Assert.AreEqual(false, p3.IsBefore(p2));
+            Assert.AreEqual(false, p3.IsBefore(p3));
+        }
+
+        [TestMethod]
+        public void Text()
+        {
+            var code = @"this is some code
+second line
+third line";
+            
+            Assert.AreEqual("", new Position(Point.START_POINT, Point.START_POINT).Text(code));
+            Assert.AreEqual("t", new Position(Point.START_POINT, new Point(1, 1)).Text(code));
+            Assert.AreEqual("this is some cod", new Position(Point.START_POINT, new Point(1, 16)).Text(code));
+            Assert.AreEqual("this is some code", new Position(Point.START_POINT, new Point(1, 17)).Text(code));
+            var e = new Position(Point.START_POINT, new Point(2, 0)).Text(code);
+            Assert.AreEqual("this is some code\r", new Position(Point.START_POINT, new Point(2, 0)).Text(code));
+            Assert.AreEqual("this is some code\r\n", new Position(Point.START_POINT, new Point(2, 1)).Text(code));
+        }
+
+        [TestMethod]
+        public void ContainsPoint()
+        {
+            var before = new Point(1, 0);
+            var start = new Point(1, 1);
+            var middle = new Point(1, 2);
+            var end = new Point(1, 3);
+            var after = new Point(1, 4);
+            var position = new Position(start, end);
+
+            Assert.IsFalse(position.Contains(before), "contains should return false with point before");
+            Assert.IsTrue(position.Contains(start), "contains should return true with point at the beginning");
+            Assert.IsTrue(position.Contains(middle), "contains should return true with point in the middle");
+            Assert.IsTrue(position.Contains(end), "contains should return true with point at the end");
+            Assert.IsFalse(position.Contains(after), "contains should return false with point after");
+        }
+
+        [TestMethod]
+        public void ContainsPosition()
+        {
+            var before = new Position(new Point(1, 0), new Point(1, 10));
+            var inside = new Position(new Point(2, 3), new Point(2, 8));
+            var after = new Position(new Point(3, 0), new Point(3, 10));
+            var position = new Position(new Point(2, 0), new Point(2, 10));
+
+            Assert.IsFalse(position.Contains(before), "contains should return false with position before");
+            Assert.IsTrue(position.Contains(position), "contains should return true with same position");
+            Assert.IsTrue(position.Contains(inside), "contains should return true with position inside");
+            Assert.IsFalse(position.Contains(after), "contains should return false with position after");
+        }
+
+        [TestMethod]
+        public void ContainsNode()
+        {
+            var before = new Position(new Point(1, 0), new Point(1, 10));
+            var inside = new Position(new Point(2, 3), new Point(2, 8));
+            var after = new Position(new Point(3, 0), new Point(3, 10));
+            var position = new Position(new Point(2, 0), new Point(2, 10));
+
+            Assert.IsFalse(position.Contains(before), "contains should return false with node before");
+            Assert.IsTrue(position.Contains(inside), "contains should return true with node inside");            
+            Assert.IsFalse(position.Contains(after), "contains should return false with node after");
+        }
+
+        [TestMethod]
+        public void ParserRuleContextPosition()
+        {
+            var code = "set foo = 123";
+            var lexer = new SimpleLangLexer(CharStreams.fromString(code));
+            var parser = new SimpleLangParser(new CommonTokenStream(lexer));
+            var cu = parser.compilationUnit();
+            var setStmt = cu.statement(0) as SimpleLangParser.SetStmtContext;
+            var pos = setStmt.ToPosition();
+            Assert.AreEqual(new Position(new Point(1, 0), new Point(1, 13)), pos);
+        }
+
+        [TestMethod]
+        public void PositionDerivedFromParseTreeNode()
+        {
+            var code = "set foo = 123";
+            var lexer = new SimpleLangLexer(CharStreams.fromString(code));
+            var parser = new SimpleLangParser(new CommonTokenStream(lexer));
+            var cu = parser.compilationUnit();
+            var setStmt = cu.statement(0) as SimpleLangParser.SetStmtContext;
+            var mySetStatement = new MySetStatement();
+            mySetStatement.Origin = new ParseTreeOrigin(setStmt);
+
+            var pos = setStmt.ToPosition();
+            Assert.AreEqual(new Position(new Point(1, 0), new Point(1, 13)), pos);
+        }
+
+        [TestMethod]
+        public void IllegalPositionAccepted()
+        {
+            var position = new Position(new Point(10, 1), new Point(5, 2), validate: false);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void IllegalPositionNotAccepted()
+        {
+            var position = new Position(new Point(10, 1), new Point(5, 2), validate: true);
+        }
+
+        [TestMethod]
+        public void ParserTreePosition()
+        {
+            var code = "set foo = 123";
+            var lexer = new SimpleLangLexer(CharStreams.fromString(code));
+            var parser = new SimpleLangParser(new CommonTokenStream(lexer));
+            var cu = parser.compilationUnit();
+            var pos = cu.ToPosition();
+            Assert.AreEqual(new Position(new Point(1, 0), new Point(1, 13)), pos);
+        }
     }
 }
--- Sharplasu/Model/Position.cs
+++ Sharplasu/Model/Position.cs
@@ -1,62 +1,57 @@
 ﻿using Antlr4.Runtime;
 using Antlr4.Runtime.Tree;
+using Strumenta.Sharplasu.Testing;
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Runtime.Serialization;
 using System.Text;
+using static Antlr4.Runtime.Atn.SemanticContext;
 
 namespace Strumenta.Sharplasu.Model
 {
     public static class PositionExtensions
-    {
-        public static Point StartPoint(this IToken token)
-        {
-            return new Point(token.Line, token.Column);
-        }
+    {        
+        public static Position Pos(int startLine, int startColumn, int endLine, int endColumn) =>
+            new Position(
+                    new Point(startLine, startColumn),
+                    new Point(endLine, endColumn)
+                );
 
-        public static Point EndPoint(this IToken token)
-        {
-            return token.StartPoint() + token.Text;
-        }
-        public static bool HasChildren(this RuleContext rule)
-        {
-            return rule.ChildCount > 0;
-        }
+        public static bool IsBefore(this Node node, Node other) => node.Position.Start.IsBefore(other.Position.Start);
 
-        public static IParseTree FirstChild(this RuleContext rule)
-        {
-            if (rule.HasChildren())
-                return rule.GetChild(0);
-            else
-                return null;
-        }
+        public static int StartLine(this Node node) => node.Position.Start.Line;
 
-        public static IParseTree LastChild(this RuleContext rule)
-        {
-            if (rule.HasChildren())
-                return rule.GetChild(rule.ChildCount - 1);
-            else
-                return null;
-        }
-
-        public static Position ToPosition(this ITerminalNode node, bool considerPosition = true)
-        {
-            if (considerPosition)
-                return new Position(node.Symbol.StartPoint(), node.Symbol.EndPoint());
-            else
-                return null;
-        }
+        public static int EndLine(this Node node) => node.Position.End.Line;
     }
 
     [Serializable]
     public class Point : IComparable<Point>
     {
-        private Point() {}
+        public static int START_LINE = 1;
+        public static int START_COLUMN = 0;
+        public static Point START_POINT = new Point(START_LINE, START_COLUMN);
+
+        private static void CheckLine(int line)
+        {
+            Asserts.Require(line >= Point.START_LINE, () => $"Line should be equal or greater than 1, was {line}");
+        }
+
+        private static void CheckColumn(int column)
+        {
+            Asserts.Require(column >= Point.START_COLUMN, () => $"Column should be equal or greater than 0, was {column}");
+        }
+
+        private Point() {}        
 
         public Point(int line, int column)
         {
             Line = line;
             Column = column;
+            CheckLine(line);
+            CheckColumn(column);
         }
 
         public int Line { get; set; }
@@ -79,12 +74,96 @@ namespace Strumenta.Sharplasu.Model
 
         public static bool operator >(Point left, Point right)
         {
-            return left.CompareTo(right) == 1;
+            return left.CompareTo(right) >= 1;
         }
 
         public static bool operator <(Point left, Point right)
         {
-            return left.CompareTo(right) == -1;
+            return left.CompareTo(right) <= -1;
+        }
+
+        public static bool operator >=(Point left, Point right)
+        {
+            return left.CompareTo(right) >= 0;
+        }
+
+        public static bool operator <=(Point left, Point right)
+        {
+            return left.CompareTo(right) <= 0;
+        }            
+
+        /**
+         * Translate the Point to an offset in the original code stream.
+         */
+        public int Offset(string code)
+        {
+            var lines = code.Split(new string[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+            Asserts.Require(lines.Length >= Line, () =>            
+                $"The point does not exist in the given text. It indicates line {Line} but there are only {lines.Length} lines"
+            );
+            Asserts.Require(lines[Line - 1].Length >= Column, 
+                () => $"The column does not exist in the given text. Line {Line} has {lines[Line - 1].Length} columns, " +
+                    $"the point indicates column {Column}"
+            );
+            var newLines = Line - 1;
+            return lines.Take(Line - 1).Sum((it) => it.Length) + newLines + Column;
+        }
+
+        /**
+         * Computes whether this point comes strictly before another point.
+         * <param name="other">the other point</param>
+         */
+        public bool IsBefore(Point other) => this < other;
+
+        /**
+         * Computes whether this point is the same as, or comes before, another point.
+         * <param name="other">the other point</param>
+         */
+        public bool IsSameOrBefore(Point other) => this <= other;
+
+        /**
+         * Computes whether this point is the same as, or comes after, another point.
+         * <param name="other">the other point</param>
+         */
+        public bool IsSameOrAfter(Point other) => this >= other;
+
+        public static Point operator +(Point point, int length)
+        {
+            return new Point(point.Line, point.Column + length);
+        }
+
+        public static Point operator +(Point point, string text)
+        {
+            if (string.IsNullOrEmpty(text))
+                return point;
+
+            var line = point.Line;
+            var column = point.Column;
+            var i = 0;
+            while(i < text.Length)
+            {
+                if (text[i] == '\n' || text[i] == '\r')
+                {
+                    line++;
+                    column = 0;
+                    if (text[i] == '\r' && i < text.Length - 1 && text[i + 1] == '\n')
+                    {
+                        i++; // Count the \r\n sequence as 1 line
+                    }
+                }
+                else
+                {
+                    column++;
+                }
+                i++;
+            }
+            return new Point(line, column);           
+        }
+
+        public Position PositionWithLength(int length)
+        {
+            Asserts.Require(length >= 0);
+            return new Position(this, this + length);
         }
 
         public Position AsPosition
@@ -94,40 +173,116 @@ namespace Strumenta.Sharplasu.Model
                 return new Position(this, this);
             }
         }
+    }    
 
-        public static Point operator +(Point point, string text)
+    [Serializable]
+    public abstract class Source { }
+
+    public class SourceSet
+    {
+        public string Name { get; private set; }
+        public string Root { get; private set; }
+
+        public SourceSet(string name, string root)
         {
-            if (string.IsNullOrEmpty(text))
-                return point;
-            else if (text.StartsWith("\r\n"))
-                return new Point(point.Line + 1, 0) + text.Substring(2);
-            else if (text.StartsWith("\n") || text.StartsWith("\r"))
-                return new Point(point.Line + 1, 0) + text.Substring(1);
-            else
-                return new Point(point.Line, point.Column + 1) + text.Substring(1);
+            Name = name;
+            Root = root;            
         }
     }
+
+    public class SourceSetElement : Source
+    {
+        public SourceSet SourceSet { get; private set; }
+        public string RelativePath { get; private set; }
+
+        public SourceSetElement(SourceSet sourceSet, string relativePath)
+        {
+            SourceSet = sourceSet;
+            RelativePath = relativePath;
+        }
+    }
+
+    public class FileSource : Source
+    {
+        public FileStream File { get; private set; }
+        
+        public FileSource(FileStream file)
+        {
+            File = file;
+        }
+    }
+
+    public class StringSource : Source
+    {
+        public string Code { get; private set; }
+
+        public StringSource(string code = null)
+        {
+            Code = Code;
+        }
+    }
+
+    public class URLSource : Source
+    {
+        public Uri Url { get; private set; }
+
+        public URLSource(Uri url)
+        {
+            Url = url;
+        }
+    }
+
+    /**
+     * This source is intended to be used for nodes that are "calculated".
+     * For example, nodes representing types that are derived by examining the code
+     * but cannot be associated to any specific point in the code.
+     *
+     * <param name="desciption">this is a description of the source. It is used to describe the process that calculated the node.
+     *                    Examples of values could be "type inference".</param>
+     */
+    public class SyntheticSource : Source
+    {
+        public string Description { get; private set; }
+
+        public SyntheticSource(string description)
+        {
+            Description = description;
+        }
+    }
+
+    /**
+     * An area in a source file, from start to end.
+     * The start point is the point right before the starting character.
+     * The end point is the point right after the last character.
+     * An empty position will have coinciding points.
+     *
+     * Consider a file with one line, containing text "HELLO".
+     * The Position of such text will be Position(Point(1, 0), Point(1, 5)).
+     */
 
     [Serializable]
     public class Position : IComparable<Position>
     {
         public Point Start { get; set; }
         public Point End { get; set; }
-
         public Source Source { get; set; } = null;
 
-        private Position() {}
+        private Position() { }
 
-        public Position(Point start, Point end, Source source = null)
+        public Position(Point start, Point end, Source source = null, bool validate = true)
         {
             Start = start;
             End = end;
             Source = source;
+            if(validate)
+            {
+                Asserts.Require(Start.IsBefore(end) || Start == End, () => $"End should follows start or be the same as start (start: {start}, end: {end})");
+            }
         }
 
         public override string ToString()
         {
-            return $"From ({Start.Line}, {Start.Column}) To ({End.Line}, {End.Column})";
+            return $"Position(start={Start.Line}, {Start.Column}), end=({End.Line}, {End.Column}) {(Source == null ? "" : $", source={Source}")}";
         }
 
         public override bool Equals(object obj)
@@ -136,12 +291,12 @@ namespace Strumenta.Sharplasu.Model
             {
                 return false;
             }
-            
+
             Position o = obj as Position;
             return ((Start == null && o.Start == null) || Start.CompareTo(o.Start) == 0) &&
                 ((End == null && o.End == null) || End.CompareTo(o.End) == 0);
         }
-        
+
         public override int GetHashCode()
         {
             unchecked
@@ -165,8 +320,64 @@ namespace Strumenta.Sharplasu.Model
                 return cmp;
             }
         }
-    }
 
-    [Serializable]
-    public abstract class Source { }
+        /**
+         * Given the whole code extract the portion of text corresponding to this position
+         */
+        public string Text(string wholeText)
+        {
+            return wholeText.Substring(Start.Offset(wholeText), End.Offset(wholeText));
+        }
+
+        /**
+         * The length in characters of the text under this position in the provided source.
+         * <param name="code">the source text.</param>
+         */
+        public int Length(string code) => End.Offset(code) - Start.Offset(code);
+
+        public bool IsEmpty => Start == End;
+
+        /**
+         * Tests whether the given point is contained in the interval represented by this object.
+         * <param name="point">the point.</param>
+         */
+        public bool Contains(Point point)
+        {
+            return ((point == Start || Start.IsBefore(point)) && (point == End || point.IsBefore(End)));
+        }
+
+        /**
+         * Tests whether the given point is contained in the interval represented by this object.
+         * <param name="point">the point.</param>
+         */
+        public bool Contains(Position position)
+        {
+            return (position != null) &&
+                Start.IsSameOrBefore(position.Start) &&
+                End.IsSameOrAfter(position.End);
+        }
+
+        /**
+         * Tests whether the given node is contained in the interval represented by this object.
+         * <param name="node">the node.</param>
+         */
+        public bool Contains(Node node)
+        {
+            return Contains(node.Position);
+        }
+
+        /**
+         * Tests whether the given position overlaps the interval represented by this object.
+         * <param name="position">the position.</param>
+         */
+        public bool Overlaps(Position position)
+        {
+            return (position != null) && (
+            (Start.IsSameOrAfter(position.Start) && Start.IsSameOrBefore(position.End)) ||
+                (End.IsSameOrAfter(position.Start) && End.IsSameOrBefore(position.End)) ||
+                (position.Start.IsSameOrAfter(Start) && position.Start.IsSameOrBefore(End)) ||
+                (position.End.IsSameOrAfter(Start) && position.End.IsSameOrBefore(End))
+            );
+        }
+    }
 }
