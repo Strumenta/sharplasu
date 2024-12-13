7a13e1db9f7ad1ec810f73cf015333b73531aef1
Gabriele Tomassetti <gabriele@strumenta.com>
2024-12-13 17:55:16 +0100

    Fix grammar generation scripts

--- Sharplasu.Tests/generate_test_parsers.sh
+++ Sharplasu.Tests/generate_test_parsers.sh
@@ -1,1 +1,1 @@
-java -cp ../antlr-4.13.0-complete.jar org.antlr.v4.Tool -Dlanguage=CSharp -Xexact-output-dir -o Generated ./antlr4/SimpleLangLexer.g4 ./antlr4/SimpleLangParser.g4
+antlr4 -Dlanguage=CSharp -Xexact-output-dir -o Generated ./antlr4/SimpleLangLexer.g4 ./antlr4/SimpleLangParser.g4
